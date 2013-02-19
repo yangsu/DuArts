7af9b810f860d3ca80ab6b29adaf5514b6d4b022
Yang Su <suyang1+services@gmail.com>
2013-02-19 02:09:14 -0500

    Temporarily fixed navigation

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.configure('development', function() {
 
 app.get('/', routes.index);
 app.get('/404', routes.notfound);
+app.get('/:page', routes.page);
+
+
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,21 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  res.render('index', {
+    title: 'Express',
+    page: 'home'
+  });
 };
 
-exports.notfound = function(req, res){
+exports.notfound = function(req, res) {
   res.render('404', { title: 'Express' });
+};
+
+exports.page = function(req, res) {
+  console.log(req.params);
+  res.render('index', {
+    title: 'Duke Arts',
+    page: req.params.page
+  });
 };
