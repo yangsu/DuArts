b34a6ca427799f68b213a4b017fa4d784ff8882e
Yang Su <suyang1+services@gmail.com>
2013-02-26 12:13:27 -0500

    Aroundme functional

--- public/javascripts/aroundme.js
+++ public/javascripts/aroundme.js
@@ -5,18 +5,40 @@ $(document).ready(function() {
     lng: -78.937401
   });
 
+  GMaps.geolocate({
+    success: function(position) {
+      var lat = position.coords.latitude,
+          lng = position.coords.longitude;
+      map.setCenter(lat, lng);
+      map.drawOverlay({
+        lat: lat,
+        lng: lng,
+        content: '<div class="overlay">You are here<div class="overlay_arrow below"></div></div>'
+      });
+    },
+    error: function(error) {
+      alert('Geolocation failed: '+error.message);
+    },
+    not_supported: function() {
+      alert("Your browser does not support geolocation");
+    },
+    always: function() {
+      // alert("Done!");
+    }
+  });
+
   $.getJSON('/markers', function(markers) {
     $.getJSON('/events', function(events) {
       _.each(events, function(event) {
-        var loc = event.location;
-        if (loc) {
-          console.log(markers[loc]);
+        var locId = event.location;
+        var loc = markers[locId];
+        if (locId && loc) {
           map.addMarker({
             lat: loc.lat,
             lng: loc.lng,
             title: loc.markerName,
             click: function(e) {
-                alert('You clicked in this marker');
+              alert('You clicked in this marker');
             }
           });
         }
