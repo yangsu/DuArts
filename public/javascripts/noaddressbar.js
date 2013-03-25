d042207aa1daa85fe738160dfba4f8054d1725bc
Yang Su <suyang1+services@gmail.com>
2013-03-25 13:59:54 -0400

    Removed noaddressbar.js. This is what broke everything...

--- public/javascripts/noaddressbar.js
+++
@@ -1,7 +0,0 @@
-if((Browser.Platform.ios) || (Browser.Platform.android) && (Browser.safari)) {
-  window.addEventListener("load",function() {
-    setTimeout(function(){
-    window.scrollTo(0, 1);
-    }, 0);
-  });
-}
