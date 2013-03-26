f0648eba887919d1247a586b37d890e5380ba4ed
Yang Su <suyang1+services@gmail.com>
2013-03-25 21:10:17 -0400

    Added calendar get data and skeleton

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -2,4 +2,25 @@ duarts.on('push', function(path) {
   if (path == '/calendar') {
     $('#calendar').datepicker();
   }
+
+  $.getJSON('/events', function(events) {
+    var transformed = _.map(events, function (event) {
+      return {
+        id: event._id,
+        name: event.summary,
+        date: {
+          start: {
+            date: event.start.shortdate,
+            time: event.start.time
+          },
+          end: {
+            date: event.end.shortdate,
+            time: event.end.time
+          }
+        },
+        description: event.description
+      };
+    });
+
+  });
 });
