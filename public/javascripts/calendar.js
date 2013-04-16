42712968eb49e83a49c50e5343f6b7c273228aad
Yang Su <suyang1+services@gmail.com>
2013-04-16 02:21:01 -0400

    Moved code to event handler on page load rather than $ function

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,46 +1,46 @@
-$(function() {
-  var calendar = $('#calendar');
-  var eventsContainer = $('#events');
+duarts.on('push', function(path) {
+  if (path == '/calendar') {
+    var calendar = $('#calendar');
+    var eventsContainer = $('#events');
 
-  var dateToEvent;
-  $.getJSON('/events', function(events) {
-    dateToEvent = _.reduce(events, function(memo, event) {
-      var date = event.start.shortdate;
-      if (memo[date]) {
-        memo[date].push(event);
-      } else {
-        memo[date] = [event];
-      }
-      return memo;
-    }, {});
-    displayEventsForDate(new Date);
-  });
-
-  function toShortDate(date) {
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var year = date.getFullYear().toString().slice(-2);
-    return month + '/' + day + '/' + year;
-  }
-
-  function displayEventsForDate(date) {
-    if (!dateToEvent) return;
-
-    date = toShortDate(date);
-    var events = dateToEvent[date];
-    var content = duarts.template('calendarEventList')({
-      date: date,
-      events: events
+    var dateToEvent;
+    $.getJSON('/events', function(events) {
+      dateToEvent = _.reduce(events, function(memo, event) {
+        var date = event.start.shortdate;
+        if (memo[date]) {
+          memo[date].push(event);
+        } else {
+          memo[date] = [event];
+        }
+        return memo;
+      }, {});
+      displayEventsForDate(new Date);
     });
-    eventsContainer.html(content);
-  }
 
-  $('#calendar').datepicker({
-    onSelect: function() {
-      var date = calendar.datepicker('getDate');
-      displayEventsForDate(date);
+    function toShortDate(date) {
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      var year = date.getFullYear().toString().slice(-2);
+      return month + '/' + day + '/' + year;
     }
-  });
 
+    function displayEventsForDate(date) {
+      if (!dateToEvent) return;
+
+      date = toShortDate(date);
+      var events = dateToEvent[date];
+      var content = duarts.template('calendarEventList')({
+        date: date,
+        events: events
+      });
+      eventsContainer.html(content);
+    }
+
+    $('#calendar').datepicker({
+      onSelect: function() {
+        var date = calendar.datepicker('getDate');
+        displayEventsForDate(date);
+      }
+    });
+  }
 });
-
