967393318fa45950746384b78e1ebe4a8ec7aaae
Yang Su <suyang1+services@gmail.com>
2013-04-17 12:43:01 -0400

    Changed features to load on page load

--- public/javascripts/features.js
+++ public/javascripts/features.js
@@ -1,15 +1,17 @@
-$(function() {
-  $('.toggle').on('toggle', function(e) {
-    var $e = $(e.currentTarget);
-    var isFeature = $e.hasClass('active');
-    var id = $e.data('id').replace(/\W+/g, '');
+duarts.on('push', function(path) {
+  if (path == '/calendar') {
+    $('.toggle').on('toggle', function(e) {
+      var $e = $(e.currentTarget);
+      var isFeature = $e.hasClass('active');
+      var id = $e.data('id').replace(/\W+/g, '');
 
-    $.ajax({
-      type: 'POST',
-      url: '/event/' + id,
-      data: {
-        feature: isFeature
-      }
+      $.ajax({
+        type: 'POST',
+        url: '/event/' + id,
+        data: {
+          feature: isFeature
+        }
+      });
     });
-  });
+  }
 });
